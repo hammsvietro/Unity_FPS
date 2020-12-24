933d260228274d3f3807202570e8208d362195d6
Pedro Hamms <pedro@vimvest.com>
2020-12-24 01:30:00 -0300

    add weapon assets and animations

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject Gun;
+    
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = Gun.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.Play("Shoot");
+        }
+    }
+}
